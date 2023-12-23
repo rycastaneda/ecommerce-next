24750560b6fcf8dd103fb514a6d122440251ca18
Ryan Castañeda <castaneda.ry@gmail.com>
2023-12-23 18:15:31 +0800

    Added spacing fixes

--- app/components/Product/Description.tsx
+++ app/components/Product/Description.tsx
@@ -12,11 +12,11 @@ const Description: FunctionComponent<DescriptionProps> = ({
 
   return (
     <section role="description" className="flex lg:flex-row flex-col">
-      <div className="lg:w-2/3 w-full">
+      <div className="lg:w-3/5 w-full">
         <p className="font-bold">{title}</p>
         {description}
       </div>
-      <div className="w-auto">
+      <div className="w-auto lg:w-2/5">
         <img className="w-full aspect-square rounded" src={thumbnail} />
       </div>
     </section>
